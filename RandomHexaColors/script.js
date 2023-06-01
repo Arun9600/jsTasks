c3d47ea800be1dad75a7cb75c448f001aca71886
Arun <69082994+Arun9600@users.noreply.github.com>
2023-06-01 11:19:02 +0530

    Modified random color codes

--- RandomHexaColors/script.js
+++ RandomHexaColors/script.js
@@ -1,7 +1,7 @@
-let ap = "0123654789ABCDEF";
 let bp = document.getElementById("cc");
 function colorGen() {
-  let cp = Math.floor(Math.random() * parseInt(ap)).toString(16);
+  let cp = Math.ceil(Math.random() * 16777216).toString(16);
   let s = "#" + cp;
   bp.innerHTML = s;
+  document.body.style.backgroundColor = s;
 }
