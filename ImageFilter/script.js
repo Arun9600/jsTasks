242b49109720f4efa86a94c93e338d19ae3b274a
Arun <69082994+Arun9600@users.noreply.github.com>
2023-06-13 12:33:44 +0530

    dom css updated

--- ImageFilter/script.js
+++ ImageFilter/script.js
@@ -1,4 +1,23 @@
 let taB = document.querySelector(".galleryTabs");
+taB.style.textAlign = "center";
+let gContainer = document.querySelector(".gallery_container");
+gContainer.style.display = "flex";
+gContainer.style.flexWrap = "wrap";
+gContainer.style.flexDirection = "row";
+let container = document.querySelector(".container");
+container.style.width = "1320px";
+container.style.margin = "0 auto";
+let btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+  btn.style.backgroundColor = "aqua";
+  btn.style.color = "#000";
+  btn.style.padding = "10px 15px";
+  btn.style.border = "none";
+  btn.style.boxShadow = "none";
+  btn.style.borderRadius = "12px";
+  btn.style.margin = "25px";
+  btn.style.cursor = "pointer";
+});
 let filter = document.querySelectorAll(".item");
 taB.addEventListener("click", (e) => {
   if (e.target.classList.contains("filter-item")) {
