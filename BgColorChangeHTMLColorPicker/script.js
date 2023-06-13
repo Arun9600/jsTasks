56987e39c8d7de80769c17c3ff0c8668fa7a96e0
Arun <69082994+Arun9600@users.noreply.github.com>
2023-06-13 12:03:35 +0530

    dom css updated

--- BgColorChangeHTMLColorPicker/script.js
+++ BgColorChangeHTMLColorPicker/script.js
@@ -1,5 +1,6 @@
 let colorPicker = document.getElementById("colorPicker");
 let colorName = document.getElementById("colorName");
+colorName.style.color = "#fff";
 colorPicker.addEventListener("input", () => {
   document.body.style.backgroundColor = colorPicker.value;
   colorName.innerHTML = `The current background color is ${colorPicker.value}`;
